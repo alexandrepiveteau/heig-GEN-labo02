57f11335ffaec429db0c381624ae15ee3830abe4
Alexandre Piveteau <alexandre.gregoire.piveteau@gmail.com>
2020-03-23 08:57:54 +0100

    N.39 Remove duplicate equality test

--- src/test/java/labo04/DollarTest.java
+++ src/test/java/labo04/DollarTest.java
@@ -3,7 +3,6 @@ package labo04;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class DollarTest {
 
@@ -12,11 +11,5 @@ public class DollarTest {
     Money five = Money.dollar(5);
     assertEquals(Money.dollar(10), five.times(2));
     assertEquals(Money.dollar(15), five.times(3));
-  }
-
-  @Test
-  public void testEquality() {
-    assertEquals(Money.dollar(5), Money.dollar(5));
-    assertNotEquals(Money.dollar(5), Money.dollar(6));
   }
 }
