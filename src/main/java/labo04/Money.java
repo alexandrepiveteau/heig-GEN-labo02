b9ba844402868a6831e8fccee49deae46e45260b
Alexandre Piveteau <alexandre.gregoire.piveteau@gmail.com>
2020-03-23 08:49:04 +0100

    N.37 Add a factory dollar method

--- src/main/java/labo04/Money.java
+++ src/main/java/labo04/Money.java
@@ -3,6 +3,10 @@ package labo04;
 public class Money {
   protected int amount;
 
+  public static Dollar dollar(int amount) {
+    return new Dollar(amount);
+  }
+
   public boolean equals(Object other) {
     Money money = (Money) other;
     return amount == money.amount && getClass().equals(money.getClass());
