19f3b28bdfe2ccdc05cd94385cd0c89a3c6139f8
Alexandre Piveteau <alexandre.gregoire.piveteau@gmail.com>
2020-03-23 11:47:03 +0100

    N.48 Fix #13

--- src/main/java/labo04/Money.java
+++ src/main/java/labo04/Money.java
@@ -21,8 +21,8 @@ public class Money {
     return currency;
   }
 
-  public Money times(int amount) {
-    return null;
+  public Money times(int multiplier) {
+    return new Money(amount * multiplier, currency);
   }
 
   public boolean equals(Object other) {
