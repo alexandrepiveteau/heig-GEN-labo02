255347decdc3f263d974da7b047428e24288295f
Alexandre Piveteau <alexandre.gregoire.piveteau@gmail.com>
2020-03-23 11:30:52 +0100

    N.43 Refactor Dollar

--- src/main/java/labo04/Dollar.java
+++ src/main/java/labo04/Dollar.java
@@ -2,12 +2,12 @@ package labo04;
 
 public class Dollar extends Money {
 
-  public Dollar(int amount) {
+  public Dollar(int amount, String currency) {
     this.amount = amount;
-    this.currency = "USD";
+    this.currency = currency;
   }
 
   public Money times(int multiplier) {
-    return new Dollar(amount * multiplier);
+    return Money.dollar(amount * multiplier);
   }
 }
