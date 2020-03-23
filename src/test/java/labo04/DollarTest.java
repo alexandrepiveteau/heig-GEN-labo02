faa7b22bd1520b498eab471148dc9990d0a336ef
Alexandre Piveteau <alexandre.gregoire.piveteau@gmail.com>
2020-03-23 08:54:28 +0100

    N.38 Remove new Dollar() calls

--- src/test/java/labo04/DollarTest.java
+++ src/test/java/labo04/DollarTest.java
@@ -2,21 +2,21 @@ package labo04;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class DollarTest {
 
   @Test
   public void testMultiplication() {
     Money five = Money.dollar(5);
-    assertEquals(new Dollar(10), five.times(2));
-    assertEquals(new Dollar(15), five.times(3));
+    assertEquals(Money.dollar(10), five.times(2));
+    assertEquals(Money.dollar(15), five.times(3));
   }
 
   @Test
   public void testEquality() {
-    assertEquals(new Dollar(5), new Dollar(5));
-    assertNotEquals(new Dollar(5), new Dollar(6));
+    assertEquals(Money.dollar(5), Money.dollar(5));
+    assertNotEquals(Money.dollar(5), Money.dollar(6));
   }
-
 }
