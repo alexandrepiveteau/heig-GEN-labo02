d820c61af39c0ec8e73485565b11d59f4328f796
Alexandre Piveteau <alexandre.gregoire.piveteau@gmail.com>
2020-03-23 13:44:54 +0100

    N.63 Fix #21 and add identity rate

--- src/main/java/labo04/Bank.java
+++ src/main/java/labo04/Bank.java
@@ -11,7 +11,8 @@ public class Bank {
   }
 
   public int rate(String from, String to) {
-    return (from.equals("CHF") && to.equals("USD")) ? 2 : 1;
+    if (from.equals(to)) return 1;
+    return rates.get(new Pair(from, to));
   }
 
   public Money reduce(Expression source, String to) {
